e9e2adf0366b5baf6c03093ecc6f3e7f7a393a10
Yuri <yurifdev@gmail.com>
2022-10-08 13:58:13 -0300

    Creating foreach function to identify numbers

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,10 @@
 "use strict";
 const numbers = document.querySelectorAll('.buttons-n');
-console.log(numbers);
+const mainDisplay = document.getElementById('current');
+numbers.forEach((num) => {
+    num.addEventListener('click', (e) => {
+        let target = e.target;
+        let num = Number(target.textContent);
+        console.log(num);
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-const numbers = document.querySelectorAll('.buttons-n'); 
-console.log(numbers);
+const numbers = document.querySelectorAll('.buttons-n');
+const mainDisplay = document.getElementById('current');
+
+//Adding the event of click
+numbers.forEach((num) => {
+    num.addEventListener('click', (e) => {
+        let target = e.target as HTMLElement;
+        let num = Number(target.textContent)
+        console.log(num);
+        // showsOnDisplay(num)
+    })
+})
+
+//Putting the number in the display
+// function showsOnDisplay(num) {
+    
+
+// }
+
