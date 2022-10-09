ccc9c62c2cc6bbf209ece50691fb546b22101c53
Yuri <yurifdev@gmail.com>
2022-10-08 22:54:22 -0300

    Making the number appears on display

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,10 +1,15 @@
 "use strict";
 const numbers = document.querySelectorAll('.buttons-n');
-const mainDisplay = document.getElementById('current');
+const mainDisplay = document.getElementById('currentDisplay');
+function showsOnDisplay(targetText) {
+    console.log(mainDisplay.innerHTML);
+    mainDisplay.innerHTML = targetText;
+}
 numbers.forEach((num) => {
     num.addEventListener('click', (e) => {
         let target = e.target;
+        let targetText = target.textContent;
         let num = Number(target.textContent);
-        console.log(num);
+        showsOnDisplay(targetText);
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 const numbers = document.querySelectorAll('.buttons-n');
-const mainDisplay = document.getElementById('current');
+const mainDisplay = document.getElementById('currentDisplay');
+
+// ! diz explicitamente o valor não é nulo ou indefinido
+// Putting the number in the display
+function showsOnDisplay(targetText : string) {
+    console.log(mainDisplay!.innerHTML);
+    mainDisplay!.innerHTML = targetText;   
+
+}
 
 //Adding the event of click
 numbers.forEach((num) => {
     num.addEventListener('click', (e) => {
         let target = e.target as HTMLElement;
+        let targetText = target.textContent;
         let num = Number(target.textContent)
-        console.log(num);
-        // showsOnDisplay(num)
+        showsOnDisplay(targetText!)
     })
 })
-
-//Putting the number in the display
-// function showsOnDisplay(num) {
-    
-
-// }
 
