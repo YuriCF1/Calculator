9585da37f55e6666f7562571e1f859949246fa98
Yuri <yurifdev@gmail.com>
2022-10-09 12:13:00 -0300

    Making the addEventListener in the operations and calling it with switch

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,15 +1,46 @@
 "use strict";
 const numbers = document.querySelectorAll('.buttons-n');
 const mainDisplay = document.getElementById('currentDisplay');
-function showsOnDisplay(targetText) {
-    console.log(mainDisplay.innerHTML);
-    mainDisplay.innerHTML = targetText;
-}
+const operator = document.querySelectorAll('[data-op]');
 numbers.forEach((num) => {
     num.addEventListener('click', (e) => {
         let target = e.target;
         let targetText = target.textContent;
         let num = Number(target.textContent);
         showsOnDisplay(targetText);
+        console.log(target);
     });
 });
+operator.forEach((op) => {
+    op.addEventListener('click', (e) => {
+        let target = e.target;
+        let targetOp = target.dataset.op;
+        switch (targetOp) {
+            case "/":
+                console.log('Dividir');
+                break;
+            case "per":
+                console.log('Porcentagem');
+                break;
+            case "root":
+                console.log('Raiz');
+                break;
+            case "+":
+                console.log('Soma');
+                break;
+            case "-":
+                console.log('Menos');
+                break;
+            case "x":
+                console.log('Multiplicação');
+                break;
+        }
+    });
+});
+function showsOnDisplay(targetText) {
+    console.log(mainDisplay.innerHTML);
+    mainDisplay.innerHTML += targetText;
+}
+function Sums(num) {
+    console.log(num);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 const numbers = document.querySelectorAll('.buttons-n');
 const mainDisplay = document.getElementById('currentDisplay');
-
-// ! diz explicitamente o valor não é nulo ou indefinido
-// Putting the number in the display
-function showsOnDisplay(targetText : string) {
-    console.log(mainDisplay!.innerHTML);
-    mainDisplay!.innerHTML = targetText;   
-
-}
+const operator = document.querySelectorAll('[data-op]');
 
 //Adding the event of click
 numbers.forEach((num) => {
@@ -16,6 +9,49 @@ numbers.forEach((num) => {
         let targetText = target.textContent;
         let num = Number(target.textContent)
         showsOnDisplay(targetText!)
+        console.log(target);
+
     })
 })
+
+operator.forEach((op) => {
+    op.addEventListener('click', (e) => {
+        let target = e.target as HTMLElement;
+        let targetOp = target.dataset.op;
+
+        switch (targetOp) {
+            case "/":
+                console.log('Dividir');
+                break
+            case "per":
+                console.log('Porcentagem');
+                break
+            case "root":
+                console.log('Raiz');
+                break
+            case "+":
+                console.log('Soma');
+                break
+            case "-":
+                console.log('Menos');
+                break
+            case "x":
+                console.log('Multiplicação');
+                break
+        }
+    })
+})
+
+// ! diz explicitamente o valor não é nulo ou indefinido
+// Putting the number in the display
+function showsOnDisplay(targetText: string) {
+    console.log(mainDisplay!.innerHTML);
+    mainDisplay!.innerHTML += targetText;
+
+}
+
+function Sums(num: string) {
+    console.log(num);
+
+}
 
