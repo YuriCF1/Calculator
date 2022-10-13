cf535237e03f0db3321de9add19d51810c331966
Yuri <yurifdev@gmail.com>
2022-10-12 23:22:24 -0300

    Counting the clicks

--- dist/js/index.js
+++ dist/js/index.js
@@ -4,28 +4,39 @@ const operator = document.querySelectorAll('[data-op]');
 const calcDisplay = document.getElementById('calc');
 const mainDisplay = document.getElementById('currentDisplay');
 let displayNumber = mainDisplay.textContent;
+var clicks = 0;
 numbers.forEach((num) => {
     num.addEventListener('click', (e) => {
         let target = e.target;
-        var targetText = target.textContent;
+        let targetText = target.textContent;
         showsOnDisplay(displayNumber, targetText);
+        console.log(clicks);
+        verifyTheCounting(clicks);
     });
 });
 operator.forEach((op) => {
     op.addEventListener('click', (e) => {
         let target = e.target;
-        var targetOp = target.dataset.op;
+        let targetOp = target.dataset.op;
+        clicks += 1;
         showsOnDisplay(displayNumber, targetOp);
-        showsCalcDisplay(mainDisplay.innerHTML);
+        console.log(clicks);
     });
 });
 function showsOnDisplay(displayNumber = '', targetInt = '') {
     mainDisplay.innerHTML += `${displayNumber}${targetInt}`;
 }
-function showsCalcDisplay(result) {
-    let cleanValue = result.substring(0, result.length - 1);
-    console.log(cleanValue);
-    let toMath = eval(cleanValue);
+function verifyTheCounting(clicks) {
+    if (clicks <= 1) {
+        makeCounts(mainDisplay.innerHTML);
+    }
+    else {
+        makeCounts(calcDisplay.innerHTML);
+    }
+}
+function makeCounts(result) {
+    console.log(result);
+    let toMath = eval(result);
     let finalResult = toMath.toString();
     calcDisplay.innerHTML = finalResult;
 }
--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,29 @@ const calcDisplay = document.getElementById('calc') as HTMLInputElement;
 const mainDisplay = document.getElementById('currentDisplay');
 
 let displayNumber = mainDisplay!.textContent;
+var clicks = 0;
 
 //Adding the event of click
 numbers.forEach((num) => {
     num.addEventListener('click', (e) => {
         let target = e.target as HTMLElement;
-        var targetText = target.textContent;
-        showsOnDisplay(displayNumber!, targetText!)
+        let targetText = target.textContent;
+        showsOnDisplay(displayNumber!, targetText!) //Manda a área e o conteúdo da tecla
+    
+        console.log(clicks);
+        verifyTheCounting(clicks)
     })
 })
 
 operator.forEach((op) => {
     op.addEventListener('click', (e) => {
         let target = e.target as HTMLElement;
-        var targetOp = target.dataset.op;
-
-        showsOnDisplay(displayNumber!, targetOp!)
-        showsCalcDisplay(mainDisplay!.innerHTML)
+        let targetOp = target.dataset.op;
+        clicks += 1;
+        
+        showsOnDisplay(displayNumber!, targetOp!);
+        // makeCounts(mainDisplay!.innerHTML) //Calcula 
+        console.log(clicks);
     })
 })
 
@@ -29,18 +35,61 @@ operator.forEach((op) => {
 // Putting the number on the display
 function showsOnDisplay(displayNumber: string = '', targetInt: string = '') {
     mainDisplay!.innerHTML += `${displayNumber}${targetInt}`;
+    
+}
 
+function verifyTheCounting(clicks : number) { //Vê se a primeira operação já foi feita
+    if (clicks <=  1) { 
+        makeCounts(mainDisplay!.innerHTML)
+    }   else {
+        makeCounts(calcDisplay!.innerHTML)
+    }
 }
 
 //Mostrando o resultado no display de cálculo
-function showsCalcDisplay(result: string) {
-    let cleanValue = result.substring(0, result.length - 1);
-    console.log(cleanValue);
-    let toMath = eval(cleanValue)
+function makeCounts(result: string) {
+    console.log(result);
+    let toMath = eval(result)
     let finalResult = toMath.toString()
     calcDisplay!.innerHTML = finalResult;
-
 }
+
+//CERTO 2_________________________________________________________
+// //Adding the event of click
+// numbers.forEach((num) => {
+//     num.addEventListener('click', (e) => {
+//         let target = e.target as HTMLElement;
+//         var targetText = target.textContent;
+//         showsOnDisplay(displayNumber!, targetText!)
+//         makeCounts(mainDisplay!.innerHTML)
+//     })
+// })
+
+// operator.forEach((op) => {
+//     op.addEventListener('click', (e) => {
+//         let target = e.target as HTMLElement;
+//         var targetOp = target.dataset.op;
+        
+//         showsOnDisplay(displayNumber!, targetOp!)
+//     })
+// })
+
+// // ! diz explicitamente o valor não é nulo ou indefinido
+// // Putting the number on the display
+// function showsOnDisplay(displayNumber: string = '', targetInt: string = '') {
+//     mainDisplay!.innerHTML += `${displayNumber}${targetInt}`;
+
+// }
+
+// //Mostrando o resultado no display de cálculo
+// function makeCounts(result: string) {
+//     // let cleanValue = result.substring(0, result.length - 1);
+//     // console.log(cleanValue);
+//     let toMath = eval(result)
+//     let finalResult = toMath.toString()
+//     calcDisplay!.innerHTML = finalResult;
+
+// }
 
 
 //CERTO___________________________________________
