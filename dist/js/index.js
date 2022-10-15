36e439764f8c790079b23101e63da13439889b1c
Yuri <yurifdev@gmail.com>
2022-10-14 23:10:01 -0300

    Starting a new logic

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,50 +1,13 @@
 "use strict";
-const numbers = document.querySelectorAll('.buttons-n');
-const operator = document.querySelectorAll('[data-op]');
-const calcDisplay = document.getElementById('calc');
-const mainDisplay = document.getElementById('currentDisplay');
-const result = document.getElementById('result');
-let displayNumber = mainDisplay.textContent;
-var clicks = 0;
-numbers.forEach((num) => {
+const numeros = document.querySelectorAll('.buttons-n');
+const operadores = document.querySelectorAll('[data-op]');
+const resultadoDiplay = document.getElementById('calc');
+const contaDisplay = document.getElementById('currentDisplay');
+const RDisplay = document.getElementById('result');
+numeros.forEach((num) => {
     num.addEventListener('click', (e) => {
         let target = e.target;
         let numeroTecla = target.textContent;
-        showsOnDisplay(displayNumber, numeroTecla, true);
-        verifyTheCounting(clicks);
-        console.log('Cliado:', clicks);
+        console.log(numeroTecla);
     });
 });
-operator.forEach((op) => {
-    op.addEventListener('click', (e) => {
-        let target = e.target;
-        let targetOp = target.dataset.op;
-        clicks += 1;
-        showsOnDisplay(displayNumber, targetOp, true);
-        verifyTheCounting(clicks);
-        console.log(clicks);
-    });
-});
-function verifyTheCounting(clicks, targetInt = '') {
-    if (clicks <= 1) {
-        makeCounts(mainDisplay.innerHTML);
-    }
-    else {
-        showsOnDisplay(mainDisplay.innerHTML, targetInt, false);
-    }
-}
-function showsOnDisplay(displayNumber = '', targetInt = '', Op) {
-    if (Op) {
-        mainDisplay.innerHTML += `${displayNumber}${targetInt}`;
-    }
-    else {
-        result.innerHTML = 'R';
-        mainDisplay.innerHTML = `${displayNumber}${targetInt}`;
-    }
-}
-function makeCounts(result, operator) {
-    console.log('Contou:', result);
-    let toMath = eval(result);
-    let finalResult = toMath.toString();
-    calcDisplay.innerHTML = finalResult;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,95 @@
-const numbers = document.querySelectorAll('.buttons-n');
-const operator = document.querySelectorAll('[data-op]');
+const numeros = document.querySelectorAll('.buttons-n');
+const operadores = document.querySelectorAll('[data-op]');
 
-const calcDisplay = document.getElementById('calc') as HTMLInputElement;
-const mainDisplay = document.getElementById('currentDisplay');
+const resultadoDiplay = document.getElementById('calc') as HTMLInputElement;
+const contaDisplay = document.getElementById('currentDisplay');
 
-const result = document.getElementById('result');
-
-
-let displayNumber = mainDisplay!.textContent;
-var clicks = 0;
+const RDisplay = document.getElementById('result'); 
 
 //Adding the event of click
-numbers.forEach((num) => {
+numeros.forEach((num) => {
     num.addEventListener('click', (e) => {
         let target = e.target as HTMLElement;
         let numeroTecla = target.textContent;
-        showsOnDisplay(displayNumber!, numeroTecla!, true) //Manda a área e o conteúdo da tecla
+        console.log(numeroTecla);
+    })
+})
+
+
+
+//_-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-_
+// const numbers = document.querySelectorAll('.buttons-n');
+// const operator = document.querySelectorAll('[data-op]');
+
+// const calcDisplay = document.getElementById('calc') as HTMLInputElement;
+// const mainDisplay = document.getElementById('currentDisplay');
+
+// const result = document.getElementById('result');
+
+
+// let displayNumber = mainDisplay!.textContent;
+// var clicks = 0;
+
+// //Adding the event of click
+// numbers.forEach((num) => {
+//     num.addEventListener('click', (e) => {
+//         let target = e.target as HTMLElement;
+//         let numeroTecla = target.textContent;
+//         showsOnDisplay(displayNumber!, numeroTecla!, true) //Manda a área e o conteúdo da tecla
     
-        verifyTheCounting(clicks);
-        console.log('Cliado:', clicks);
-    })
-})
+//         verifyTheCounting(clicks);
+//         console.log('Cliado:', clicks);
+//     })
+// })   
 
-operator.forEach((op) => {
-    op.addEventListener('click', (e) => {
-        let target = e.target as HTMLElement;
-        let targetOp = target.dataset.op;
-        clicks += 1;
+// operator.forEach((op) => {
+//     op.addEventListener('click', (e) => {
+//         let target = e.target as HTMLElement;
+//         let targetOp = target.dataset.op;
         
-        showsOnDisplay(displayNumber!, targetOp!, true);
-        verifyTheCounting(clicks);
-        console.log(clicks);
-    })
-})
+//         verifyTheCounting(clicks);
+//         showsOnDisplay(displayNumber!, targetOp!, true);
+//         console.log(clicks);
+//         clicks += 1;
+//     })
+// })
 
-function verifyTheCounting(clicks : number, targetInt: string = '') { //Vê se a primeira operação já foi feita
-    if (clicks <=  1) { 
-        makeCounts(mainDisplay!.innerHTML)
-    } else {
-        showsOnDisplay(mainDisplay!.innerHTML, targetInt, false)
-    }
-}
+// function verifyTheCounting(clicks : number, targetInt: string = '') { //Vê se a primeira operação já foi feita
+//     if (clicks <=  1) { 
+//         makeCounts(mainDisplay!.innerHTML)
+//     } else {
+//         mainDisplay!.innerHTML = ''
+//         showsOnDisplay(mainDisplay!.innerHTML, targetInt, false)
+//         clicks = 0;
+//     }
+// }
 
-// ! diz explicitamente o valor não é nulo ou indefinido
-// Putting the number on the display
-function showsOnDisplay(displayNumber: string = '', targetInt: string = '', Op: boolean) {
-    if (Op) {
-        mainDisplay!.innerHTML += `${displayNumber}${targetInt}`;
-    } else {
-        result!.innerHTML = 'R'
-        mainDisplay!.innerHTML = `${displayNumber}${targetInt}`;
-    }
-}
+// function a (a: any, b: any) {
+//         console.log(a,b);
+// }
 
-//Mostrando o resultado no display de cálculo
-function makeCounts(result: string, operator? : string) {
-    console.log('Contou:',result);
-        let toMath = eval(result)
-        let finalResult = toMath.toString()
-        calcDisplay!.innerHTML = finalResult;
 
-}
+// // ! diz explicitamente o valor não é nulo ou indefinido
+// // Putting the number on the display
+// function showsOnDisplay(displayNumber: string = '', targetInt: string = '', Op: boolean) {
+//     if (Op) {
+//         mainDisplay!.innerHTML += `${displayNumber}${targetInt}`;
+//     } else {
+//         result!.innerHTML = 'R'
+//         mainDisplay!.innerHTML = `${displayNumber}${targetInt}`;
+//     }
+// }
+
+// //Mostrando o resultado no display de cálculo
+// function makeCounts(result: string, operator? : string) {
+//     console.log('Contou:',result);
+//         let toMath = eval(result)
+//         let finalResult = toMath.toString()
+//         calcDisplay!.innerHTML = finalResult;
+
+// }
+
+//_-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-_
 
 //CERTO 2_________________________________________________________
 // //Adding the event of click
